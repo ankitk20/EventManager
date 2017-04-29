7a5d5e4d55c568c81c5f26f98d505e1c75f52358
raghavjhawar <raghav.zawar007@gmail.com>
2017-04-29 14:12:30 +0530

    login bug removed

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "eventmanager";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+ 	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}else{
+		echo "conn success";
+	}
+
+	$userid = $_POST["user"];
+	$password = $_POST["pass"];
+	echo $UserID;
+	if(isset($_POST['user'])){
+		echo "username set";
+	}else{
+		echo "username not set";
+	}		
+
+
+	$sql = "SELECT * FROM login where userid = '$userid' and password = '$password'";
+
+	$result = mysqli_query($conn, $sql);
+
+	if ($result->num_rows ==1){ 
+         $_SESSION['user'] = $UserID;
+          $_SESSION["loggedin"] = true;
+         header("location: addEvent.html");
+	}
+	else {
+	    echo "<script>alert('Invalid User Name or Password');window.location.href = './login.html'</script>";
+	}
+	$conn->close();
+?>
